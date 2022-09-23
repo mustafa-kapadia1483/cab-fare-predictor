ba7d33780253b492b999fc4a52dcd3a0ab2cf00e
Mustafa Kapadia <mustafakapadia76@gmail.com>
2022-09-23 17:12:15 +0530

    deploy fixes

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,5 @@
-import Head from "next/head";
 import MapContainer from "../components/map/MapContainer";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.0/mapbox-gl-directions.js"></script>
-        <link
-          rel="stylesheet"
-          href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.0/mapbox-gl-directions.css"
-          type="text/css"
-        />
-      </Head>
-      <MapContainer />;
-    </>
-  );
+  return <MapContainer />;
 }
